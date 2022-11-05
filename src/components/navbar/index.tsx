29183893b422d1dc022586254913a84678706f38
napat-CoderNo1 <f1.2544pat@gmail.com>
2022-11-05 15:26:33 +0700

    finished startpage and navbar com

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,15 +2,14 @@ import React, { useState } from 'react';
 import './navbar.css';
 import logo from 'assets/images/logo.png';
 import { Link } from 'react-router-dom';
-
 import { Divide as Hamburger } from 'hamburger-react';
+import { SidebarData } from './sidebarData';
 
 const NavBar = () => {
   const [isOpen, setOpen] = useState(false);
 
   const [sidebar, setSidebar] = useState(false);
-  const showSideBar = () => setSidebar(!sidebar);
-
+  const showSidebar = () => setSidebar(!sidebar);
   return (
     <div>
       <nav>
@@ -23,15 +22,31 @@ const NavBar = () => {
             </div>
             <div className='separateLogoLine'></div>
             <div className='project-name'>
-              <p className='font-link'>DOOHEE</p>
+              <p className='font-link'>DOOHOR</p>
             </div>
           </div>
-          <div className='hamburger' onClick={showSideBar}>
+          <div className='menu-bars' onClick={showSidebar}>
             <Hamburger color='#15cd64' toggled={isOpen} rounded toggle={setOpen} />
           </div>
         </div>
-        <div className={sidebar ? 'sidebar openbar' : ' sidebar closebar'}></div>
       </nav>
+      <div className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+        <ul className='nav-munu-items'>
+          <li className='navbar-toggle'>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    <div className='itemX'>
+                      <span className='font-link'>{item.title}</span>
+                    </div>
+                  </Link>
+                </li>
+              );
+            })}
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
