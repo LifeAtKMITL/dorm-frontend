93c46d718d880600c9bef732b43a15e7bdb62262
napat-CoderNo1 <f1.2544pat@gmail.com>
2022-11-12 18:29:33 +0700

    test filter

--- src/pages/custom/index.tsx
+++ src/pages/custom/index.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from 'components/navbar';
 import './custom.css';
-
+import Navbar from 'components/navbar';
+import Zone from 'components/customPageComponent/zone';
 const Custom = () => {
   return (
-    <div className='custompage-container'>
-      <NavBar />
+    <div className='custom-container'>
+      <Zone />
     </div>
   );
 };
