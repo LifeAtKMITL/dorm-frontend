6e7975021b0c1c3b58108f7392a516385de19440
zantaclaus <tarzuto@gmail.com>
2022-11-12 22:35:15 +0700

    Implement AuthContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import AuthProvider from 'contexts/auth/AuthProvider';
 import React from 'react';
-
+import { LiffProvider } from 'react-liff';
 import Routes from 'routes';
 
 function App() {
-  return <Routes />;
-
+  return (
+    <LiffProvider liffId='1657631189-9wk8vxJA'>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </LiffProvider>
+  );
 }
 
 export default App;
