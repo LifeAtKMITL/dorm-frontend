96342e2d44c7ef7399a9140fcfafb978eab72282
napat-CoderNo1 <f1.2544pat@gmail.com>
2022-11-14 14:43:21 +0700

    custom v1

--- src/components/customPageComponent/zone/index.tsx
+++ src/components/customPageComponent/zone/index.tsx
@@ -3,14 +3,15 @@ import ZoneButton from '../zoneButton';
 import './zone.css';
 
 const Zone = () => {
+  // Zone-Button
   const dormName1 = 'เกกี';
-  const dormName2 = 'เกกี';
-  const dormName3 = 'เกกี';
+  const dormName2 = 'RNP';
+  const dormName3 = 'ซอยหอใหม่';
   return (
     <div className='zone_button-container'>
-      <ZoneButton />
-      <ZoneButton />
-      <ZoneButton />
+      <ZoneButton dn={dormName1} />
+      <ZoneButton dn={dormName2} />
+      <ZoneButton dn={dormName3} />
     </div>
   );
 };
--- src/components/customPageComponent/zoneButton/index.tsx
+++ src/components/customPageComponent/zoneButton/index.tsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
 import './zoneButton.css';
 
-const ZoneButton = () => {
+const ZoneButton = (props: any) => {
+  // Zone-Button
+  const { dn } = props;
+  const dormName = dn;
   const [btnState, setBtnState] = useState(false);
-
   function handleClick() {
     setBtnState((btnState) => !btnState);
   }
-
   let toggleClassCheck = btnState ? ' active' : ' ';
 
-  return <button className={`zone-btn${toggleClassCheck}`} onClick={handleClick}></button>;
+  return (
+    <button className={`zone-btn${toggleClassCheck}`} onClick={handleClick}>
+      {dormName}
+    </button>
+  );
 };
 
 export default ZoneButton;
